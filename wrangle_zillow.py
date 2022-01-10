9e09b3de3d0e30849c9dc0e520a9a72e60c937a2
Chloe Whitaker <chloerwhitaker@gmail.com>
2022-01-09 20:56:59 -0600

    added cluster function

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -18,6 +18,7 @@ from sklearn.model_selection import train_test_split
 
 # Scale
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 import sklearn.preprocessing
 
 # Model
@@ -553,3 +554,39 @@ def model_test(X_train_model, y_train, y_test, X_test_model):
     rmse_test_glm = round(mean_squared_error(y_test.logerror, y_test.logerror_pred_glm)**(1/2), 5)
 
     return rmse_test_glm
+
+    # Clustering
+
+    # functions to create clusters and scatter-plot:
+
+
+def create_cluster(train, X, k):
+    
+    """ Takes in df, X (dataframe with variables you want to cluster on) and k
+    # It scales the X, calcuates the clusters and return train (with clusters), the Scaled dataframe,
+    #the scaler and kmeans object and unscaled centroids as a dataframe"""
+    
+    scaler = StandardScaler(copy=True).fit(X)
+    X_scaled = pd.DataFrame(scaler.transform(X), columns=X.columns.values).set_index([X.index.values])
+    kmeans = KMeans(n_clusters = k, random_state = 42)
+    kmeans.fit(X_scaled)
+    kmeans.predict(X_scaled)
+    train['cluster'] = kmeans.predict(X_scaled)
+   
+#     validate['cluster'] = kmeans.predict(X_scaled)
+   
+#     test['cluster'] = kmeans.predict(X_scaled)
+   
+    centroids = pd.DataFrame(scaler.inverse_transform(kmeans.cluster_centers_), columns=X_scaled.columns)
+    return train, X_scaled, scaler, kmeans, centroids
+
+
+def create_scatter_plot(x,y,df,kmeans, X_scaled, scaler):
+    
+    """ Takes in x and y (variable names as strings, along with returned objects from previous
+    function create_cluster and creates a plot"""
+    
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x = x, y = y, data = df, hue = 'cluster')
+    centroids = pd.DataFrame(scaler.inverse_transform(kmeans.cluster_centers_), columns=X_scaled.columns)
+    centroids.plot.scatter(y=y, x= x, ax=plt.gca(), alpha=.30, s=500, c='black')
